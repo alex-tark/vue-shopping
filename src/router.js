d7e391766300bfe30523ec6a84257a035ee919c4
Alexander Zinin <azinin@dengabank.ru>
2018-07-09 19:43:03 +0300

    add empty sign in view component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SignIn from './views/SignIn.vue'
 import ErrorPage from './views/Error.vue'
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home,
+        },
+        {
+            path: '/sign-in',
+            name: 'sign-in',
+            component: SignIn
         },
         {
             path: '/*',
---
+++ src/views/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login pv4">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sign-in'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
