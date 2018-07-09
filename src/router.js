574b1dca5a483b91f97ed775111e5f230c0a9918
Alexander Zinin <azinin@dengabank.ru>
2018-07-09 20:56:43 +0300

    remove basic views and add empty sign up page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import SignIn from './views/SignIn.vue'
+import SignUp from './views/SignUp.vue'
 import ErrorPage from './views/Error.vue'
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ export default new Router({
             path: '/sign-in',
             name: 'sign-in',
             component: SignIn
+        },
+        {
+            path: '/sign-up',
+            name: 'sign-up',
+            component: SignUp
         },
         {
             path: '/*',
---
+++ src/views/SignUp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pv4">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'sign-up'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
