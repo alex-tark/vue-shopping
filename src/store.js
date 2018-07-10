d085a8bd3e807aa598b9058a6be46ff1685e0e9d
Alexander Zinin <azinin@dengabank.ru>
2018-07-10 10:43:22 +0300

    code review

--- src/store.js
+++ src/store.js
@@ -25,7 +25,7 @@ export default new Vuex.Store({
     state: {
         isSearching: false,
         searchQuery: '',
-        siteData: {},
+        siteData: [],
         filteredComponents: [],
         hasResults: false,
         activeComponent: null
